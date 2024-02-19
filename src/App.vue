6aacbdd05bd07836a9ad25ea7730b11480253f0b
kellyericang <kelly.ng1@outlook.com>
2024-02-18 21:15:20 -0500

    feat: add end of round card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import masterList from "./components/masterList.json"
+import EndRoundCard from "./components/EndRoundCard.vue"
 import ScoreBoard from "./components/ScoreBoard.vue"
 import SymptomList from "./components/SymptomList.vue"
 import Lives from "./components/Lives.vue"
@@ -12,8 +13,11 @@ const livesLost = ref(0);
 const score = ref({ correct: 0, incorrect: 0 });
 const gameStarted = ref(false);
 const guess = ref("");
+const guessesMade = ref([]);
+const win = ref(false);
 
 function skip() {
+  guessesMade.value.push("skipped");
   livesLost.value++;
   if (livesLost.value === 4) {
     livesLost.value = 0;
@@ -22,6 +26,7 @@ function skip() {
 }
 
 function rightAnswer() {
+  win.value = true;
   score.value.correct++;
 }
 
@@ -43,25 +48,27 @@ function getMedicalCondition() {
   currentCondition.value = masterConditionList.value[rng];
 }
 function makeGuess() {
+  guessesMade.value.push(guess.value);
   if (guess.value === currentCondition.value.name) {
     // correct guess
     rightAnswer();
-    reset();
   } else if (livesLost.value === 2) {
     // wrong guess and lose
     wrongAnswer();
-    reset();
+    livesLost.value++;
   } else {
     // wrong guess
     livesLost.value++;
-    guess.value = "";
   }
+  guess.value = "";
 }
 
 function reset() {
+  win.value = false;
   getMedicalCondition();
   livesLost.value = 0;
   guess.value = "";
+  guessesMade.value = [];
 }
 
 onMounted(() => {
@@ -72,27 +79,45 @@ onMounted(() => {
 <template>
   <main>
     <div v-if="!gameStarted">
+      <h1>Diagnosis Dash</h1>
       <div class="rules">
         guess the medical condition based on the symptoms shown
       </div>
       <button @click="gameStarted = true">START GAME</button>
     </div>
-    <div v-else> 
+    <div v-else class="game-board"> 
       <button @click="gameStarted = false">BACK</button>
       <ScoreBoard :score-correct="score.correct" :score-incorrect="score.incorrect" />
       <SymptomList :symptoms="currentCondition.symptoms" :guess-number="livesLost" />
       <Lives :lives-lost="livesLost"/>
+      Guesses Made
+      <ol>
+        <li v-for="guessMade in guessesMade" :key="guessMade">
+          {{ guessMade }}
+        </li>
+      </ol>
       <button @click="skip">
         SKIP
       </button>
       <input type="text" v-model="guess" @keypress.enter="makeGuess"/>
       <button @click="makeGuess">GUESS</button>
+      <EndRoundCard
+        v-if="win || livesLost === 3"
+        :correct="win"
+        :right-answer="currentCondition.name"
+        :guesses-made="guessesMade"
+        @next-round="reset"
+      />
     </div>
   </main>
 </template>
 
 <style scoped>
 main {
+  display: flex;
+  flex-direction: column;
+}
+.game-board {
   display: flex;
   flex-direction: column;
 }
---
+++ src/components/EndRoundCard.vue
@@ -0,0 +1,67 @@
+<script setup>
+defineProps({
+  correct: {
+    type: Boolean,
+    required: true
+  },
+  rightAnswer: {
+    type: String,
+    required: true
+  },
+  guessesMade: {
+    type: Array,
+    required: true
+  }
+})
+
+</script>
+
+<template>
+  <div class="end-round-screen">
+    <div class="result">{{ correct ? "Correct!" : "Incorrect" }}</div>
+    <div class="condition">
+      The condition was {{ rightAnswer }}
+    </div>
+    <ol>
+      Your Guesses
+      <li v-for="guessMade in guessesMade" :key="guessMade">
+        {{ guessMade }}
+      </li>
+    </ol>
+    <button @click="$emit('next-round')">NEXT ROUND</button>
+  </div>
+</template>
+
+<style scoped>
+.end-round-screen {
+  position: fixed;
+  height: 50dvh;
+  width: 80vw;
+  max-width: 1080px;
+  max-height: 500px;
+  background: rgb(255 255 255 / 0.6);
+  backdrop-filter: blur(20px);
+  border-radius: 20px;
+  left: 10vw;
+  top: 10dvh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: black;
+  justify-content: space-around;
+  padding: 48px;
+}
+.result {
+  font-weight: 600;
+  font-size: 4rem;
+  color: black;
+}
+.condition {
+  font-weight: 600;
+  font-size: 2rem;
+  color: black;
+}
+ol {
+  font-size: 1rem;
+}
+</style>
