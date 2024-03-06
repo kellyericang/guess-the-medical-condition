018a3a44445c9e462a0dccfa962ded60af2c7c70
kellyericang <kelly.ng1@outlook.com>
2024-03-05 23:28:06 -0500

    feat: ddx fill in the blanks

--- src/components/ddx/ddxGameBoard.vue
+++ src/components/ddx/ddxGameBoard.vue
@@ -1,54 +1,100 @@
 <script setup>
-import masterList from "../masterList.json"
-import { ref, watch } from "vue"
+import masterList from '../masterList.json'
+import { ref, watch } from 'vue'
 
-const masterConditionList = ref(masterList.primary_complaints);
-const currentPrimaryComplaint = ref({ name: "", systems: [] });
-const currentConditionIndex = ref(-1);
-const livesLost = ref(0);
-let randomOrder = ref([]);
+const masterConditionList = ref(masterList.primary_complaints)
+const currentPrimaryComplaint = ref({ name: '', systems: [] })
+const currentConditionIndex = ref(-1)
+let randomOrder = ref([])
+let randomOrderIndex = ref(0)
+let answers = ref([])
+let guess = ref('')
 
 function generateSystemsOrder() {
-  console.log(currentPrimaryComplaint.value.systems.length);
-  const numArray = [];
+  const numArray = []
   for (let i = 0; i < currentPrimaryComplaint.value.systems.length; i++) {
-    numArray.push(i);
+    numArray.push(i)
   }
   for (let i = numArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [numArray[i], numArray[j]] = [numArray[j], numArray[i]];
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[numArray[i], numArray[j]] = [numArray[j], numArray[i]]
   }
-  randomOrder.value = numArray;
-  console.log(randomOrder.value);
+  randomOrder.value = numArray
+}
+
+function resetAnswers() {
+  answers.value = []
+  for (const x in currentPrimaryComplaint.value.systems) {
+    let system = []
+    for (const y in currentPrimaryComplaint.value.systems[x].conditions) {
+      system.push(false)
+    }
+    answers.value.push(system)
+  }
+}
+
+function makeGuess() {
+  const index = currentPrimaryComplaint.value.systems[
+    randomOrder.value[randomOrderIndex.value]
+  ].conditions.indexOf(guess.value)
+  if (index !== -1) {
+    answers.value[randomOrder.value[randomOrderIndex.value]][index] = true
+    guess.value = ''
+  }
+}
+
+function showAnswers() {
+  for (const x in answers.value[randomOrder.value[randomOrderIndex.value]]) {
+    answers.value[randomOrder.value[randomOrderIndex.value]][x] = true
+  }
 }
 
 watch(currentPrimaryComplaint, () => {
-  if (currentPrimaryComplaint.value.name !== "") {
-    generateSystemsOrder();
+  if (currentPrimaryComplaint.value.name !== '') {
+    generateSystemsOrder()
+    resetAnswers()
   }
 })
-
 </script>
 
 <template>
-  <div class="ddx-game-board"> 
+  <div class="ddx-game-board">
     <div v-if="currentPrimaryComplaint.name === ''">
       <h1>Pick a Condition</h1>
-      <div v-for="symptom in masterConditionList">
+      <div v-for="symptom in masterConditionList" :key="symptom">
         <button @click="currentPrimaryComplaint = symptom">{{ symptom.name }}</button>
       </div>
     </div>
     <div v-else>
       <h1>Complete the ddx for: {{ currentPrimaryComplaint.name }}</h1>
-      <div v-for="num in randomOrder">
-        {{ currentPrimaryComplaint.systems[randomOrder[num]].name }}
-        <div v-for="condition in currentPrimaryComplaint.systems[randomOrder[num]].conditions">
-          {{ condition }}
-        </div>
+      <h2>{{ currentPrimaryComplaint.systems[randomOrder[randomOrderIndex]].name }}</h2>
+      <div class="buttons">
+        <button :disabled="randomOrderIndex === 0" @click="randomOrderIndex--">PREVIOUS</button>
+        <button :disabled="randomOrderIndex === randomOrder.length - 1" @click="randomOrderIndex++">
+          NEXT
+        </button>
+        <button @click="showAnswers">SHOW ANSWERS</button>
+      </div>
+      <input v-model="guess" type="text" class="input-guess" @keyup="makeGuess" />
+      <div
+        v-for="(condition, index) in currentPrimaryComplaint.systems[randomOrder[randomOrderIndex]]
+          .conditions"
+        :key="condition"
+      >
+        {{ answers[randomOrder[randomOrderIndex]][index] === false ? '????????????' : condition }}
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.ddx-game-board {
+  padding: 16px;
+}
+.buttons {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 16px 0px;
+}
 </style>
