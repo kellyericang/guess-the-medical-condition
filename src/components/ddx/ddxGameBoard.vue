a8ef58da492f6f0924d102ca807693eea3fb24f0
kellyericang <kelly.ng1@outlook.com>
2024-03-07 20:53:30 -0500

    feat: more buttons and 2 more symptoms

--- src/components/ddx/ddxGameBoard.vue
+++ src/components/ddx/ddxGameBoard.vue
@@ -60,12 +60,15 @@ watch(currentPrimaryComplaint, () => {
 <template>
   <div class="ddx-game-board">
     <div v-if="currentPrimaryComplaint.name === ''">
-      <h1>Pick a Condition</h1>
-      <div v-for="symptom in masterConditionList" :key="symptom">
-        <button @click="currentPrimaryComplaint = symptom">{{ symptom.name }}</button>
-      </div>
+      <label for="symptom-select">Pick a symptom: </label>
+      <select name="symptoms" id="symptom-select" v-model="currentPrimaryComplaint">
+        <option v-for="symptom in masterConditionList" :key="symptom" :value="symptom">
+          {{ symptom.name }}
+        </option>
+      </select>
     </div>
     <div v-else>
+      <button @click="currentPrimaryComplaint = { name: '', systems: [] }">change topic</button>
       <h1>Complete the ddx for: {{ currentPrimaryComplaint.name }}</h1>
       <h2>{{ currentPrimaryComplaint.systems[randomOrder[randomOrderIndex]].name }}</h2>
       <div class="buttons">
@@ -73,15 +76,17 @@ watch(currentPrimaryComplaint, () => {
         <button :disabled="randomOrderIndex === randomOrder.length - 1" @click="randomOrderIndex++">
           NEXT
         </button>
-        <button @click="showAnswers">SHOW ANSWERS</button>
+        <button @click="showAnswers">SHOW ALL ANSWERS</button>
       </div>
-      <input v-model="guess" type="text" class="input-guess" @keyup="makeGuess" />
+      <input v-model="guess" type="text" class="input-guess" placeholder="your guess here" @keyup="makeGuess" />
       <div
         v-for="(condition, index) in currentPrimaryComplaint.systems[randomOrder[randomOrderIndex]]
           .conditions"
         :key="condition"
+        class="answer-row"
       >
         {{ answers[randomOrder[randomOrderIndex]][index] === false ? '????????????' : condition }}
+        <button :disabled="answers[randomOrder[randomOrderIndex]][index]" @click="answers[randomOrder[randomOrderIndex]][index] = true"> show answer </button>
       </div>
     </div>
   </div>
@@ -96,5 +101,12 @@ watch(currentPrimaryComplaint, () => {
   width: 100%;
   justify-content: space-between;
   padding: 16px 0px;
+}
+.answer-row {
+  display: flex;
+  justify-content: space-between;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  border-bottom: solid 2px rgb(0 0 0 / 0.5);
 }
 </style>
